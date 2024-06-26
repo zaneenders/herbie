d87a728e42feecf55cfd25d37ffce32b1875bb80
AYadrov <kowalski228@mail.ru>
2024-06-26 15:46:29 -0600

    histograms script update

--- plots/histograms.py
+++ plots/histograms.py
@@ -1,21 +1,24 @@
 import numpy as np
+import requests
 from matplotlib import pyplot as plt
 import pandas as pd
 import json
 import argparse
 
+def load_mixsample(url, tool):
+    timeline_json = requests.get(url).json()
+    # outcomes = timeline_json[0]["mixsample-" + str(tool)]
+    outcomes = timeline_json[0]["mixsample"]
+    outcomes = pd.DataFrame(outcomes, columns=['time', 'op', 'precision'])
+    return outcomes
 
 def plot_histograms(args):
-    baseline_json = json.load(open(args.baseline))
-    baseline = pd.DataFrame(baseline_json, columns=['time', 'op', 'precision'])
+    baseline = load_mixsample(args.timeline, "base")
+    rival = load_mixsample(args.timeline, "rival")
 
-    rival_json = json.load(open(args.rival))
-    rival = pd.DataFrame(rival_json, columns=['time', 'op', 'precision'])
-
-    fig, ax = plt.subplots(3, 1, figsize=(5.5, 6))
-    plot_histogram_for_operation(op_name="ival-sin", ax=ax[0], rival=rival, baseline=baseline)
-    plot_histogram_for_operation(op_name="ival-add", ax=ax[1], rival=rival, baseline=baseline)
-    plot_histogram_for_operation(op_name="ival-sub", ax=ax[2], rival=rival, baseline=baseline)
+    fig, ax = plt.subplots(len(args.ops), 1, figsize=(5.5, 2*len(args.ops)))
+    for i, op in enumerate(args.ops):
+        plot_histogram_for_operation(op_name=op, ax=ax[i], rival=rival, baseline=baseline)
 
     handles, labels = ax[0].get_legend_handles_labels()
     fig.legend(handles, labels)
@@ -24,7 +27,6 @@ def plot_histograms(args):
 
 
 def bucket_precisions_by_bins(data, bins):
-    total_time = data["time"].sum()
     x = [0] * len(bins)
     for i in range(len(bins) - 1):
         time_per_bucket = data.loc[(data["precision"] >= bins[i]) & (data["precision"] < bins[i + 1]), "time"].sum()
@@ -45,7 +47,6 @@ def plot_histogram_for_operation(op_name="ival-sin", ax=None, rival=None, baseli
     ax.bar(np.arange(len(bins)) + 0.6, buckets_rival, color="red", alpha=0.7, width=0.6, label='rival')
     ax.yaxis.grid(True, linestyle='-', which='major', color='grey', alpha=0.3)
 
-
     ax.set_xticks(np.arange(len(bins)), bins)
     ax.set_xticklabels([str(x) if x % 1000 == 0 else "" for x in bins])
 
@@ -54,11 +55,12 @@ def plot_histogram_for_operation(op_name="ival-sin", ax=None, rival=None, baseli
     ax.set_xlabel("Precision (number of bits)")
     ax.set_title("Precision tuning for " + op_name)
 
-
 parser = argparse.ArgumentParser(
-                    prog='histograms.py',
-                    description='Script outputs mixed precision histograms for a Herbie run')
-parser.add_argument('-c', '--rival', default="rival.json")
-parser.add_argument('-v', '--baseline', default="baseline.json")
+    prog='histograms.py',
+    description='Script outputs mixed precision histograms for a Herbie run')
+parser.add_argument('-t', '--timeline-url', dest='timeline', default="https://nightly.cs.washington.edu/reports/herbie/1719410524:nightly:main:aefdd770dd/timeline.json")
+parser.add_argument('-o', '--ops', action='store', dest='ops',
+                    type=str, nargs='*', default=['ival-sin', 'ival-tan', 'ival-cos', 'ival-pow'],
+                    help="Examples: -o ival-sin ival-sub ival-add")
 args = parser.parse_args()
 plot_histograms(args)
