6ae0f9c13a682499e2f5b78dc61522ec9ffe1e78
AYadrov <kowalski228@mail.ru>
2024-07-02 16:18:01 -0600

    few new graphs

---
+++ plots/cdf.py
@@ -0,0 +1,35 @@
+import numpy as np
+import requests
+import matplotlib.pyplot as plt
+from matplotlib.ticker import *
+import pandas as pd
+import json
+import argparse
+
+def load_mixsample(url, tool):
+    timeline_json = requests.get(url).json()
+    outcomes = timeline_json[0]["mixsample-" + str(tool)]
+    outcomes = pd.DataFrame(outcomes, columns=['time', 'op', 'precision'])
+    return outcomes
+
+
+def plot_cdf(args):
+    baseline = load_mixsample(args.timeline, "base").sort_values(by=['precision']).groupby(by='precision').sum().cumsum().reset_index()
+    rival = load_mixsample(args.timeline, "rival").sort_values(by=['precision']).groupby(by='precision').sum().cumsum().reset_index()
+
+    fig, ax = plt.subplots(figsize=(4, 3.5))
+    ax.plot(baseline['precision'], np.array(baseline['time'])/1000, linewidth=2, color='g', label='baseline')
+    ax.plot(rival['precision'], np.array(rival['time'])/1000, linewidth=2, color='red', label='rival')
+    ax.set_xscale('log', base=2)
+    ax.yaxis.grid(True, linestyle='-', which='major', color='grey', alpha=0.3)
+    ax.set_xlabel("Precision (number of bits)")
+    ax.set_ylabel("Seconds spent")
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig("cdf.pdf", format="pdf")
+
+
+parser = argparse.ArgumentParser(prog='histograms.py', description='Script outputs mixed precision histograms for a Herbie run')
+parser.add_argument('-t', '--timeline-url', dest='timeline', default="https://nightly.cs.washington.edu/reports/herbie/1719841805:nightly:artem-popl-eval-histograms:d87a728e42/timeline.json")
+args = parser.parse_args()
+plot_cdf(args)
---
+++ plots/ratio_plot.py
@@ -0,0 +1,47 @@
+import argparse
+
+import requests
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_speed_graph(outcomes, ax):
+    baseline_cmp = outcomes.loc[outcomes['tool_name'] == "valid-baseline"]
+    rival_cmp = outcomes.loc[outcomes['tool_name'] == "valid-rival"]
+    sollya_cmp = outcomes.loc[outcomes['tool_name'] == "valid-sollya"]
+
+    def add_values(row):
+        return int(row['rival_iter']) + 1, (row['number_of_points'] / row['time']) * 1000
+
+    def tool_cmp2speed(x):
+        return x.sort_values(by=['rival_iter']).apply(add_values, axis=1, result_type='expand')
+
+    base = np.array(tool_cmp2speed(sollya_cmp)[1][:5])
+
+    ax.plot(tool_cmp2speed(baseline_cmp)[0][:5], np.array(tool_cmp2speed(baseline_cmp)[1][:5])/base, '.-', linewidth=2.0, color='g',
+            label='baseline')
+    ax.plot(tool_cmp2speed(rival_cmp)[0][:5], np.array(tool_cmp2speed(rival_cmp)[1][:5])/base, '.-', linewidth=2.0, color='r', label='rival')
+    ax.plot(tool_cmp2speed(sollya_cmp)[0][:5], np.array(tool_cmp2speed(sollya_cmp)[1][:5])/base, '.-', linewidth=2.0, color='b',
+            label='sollya')
+
+    ax.legend()
+    ax.set_xlabel("Difficulty")
+    ax.set_ylabel("Ratio to baseline")
+    ax.yaxis.grid(True, linestyle='-', which='major', color='grey', alpha=0.3)
+
+def load_outcomes(url):
+    timeline_json = requests.get(url).json()
+    outcomes = timeline_json[0]["outcomes"]
+    outcomes = pd.DataFrame(outcomes, columns=['time', 'rival_iter', 'tool_name', 'number_of_points'])
+    return outcomes
+
+parser = argparse.ArgumentParser(prog='histograms.py', description='Script outputs mixed precision histograms for a Herbie run')
+parser.add_argument('-t', '--timeline', dest='timeline', default="https://nightly.cs.washington.edu/reports/herbie/1719948723:nightly:artem-popl-eval:db6536e941/timeline.json")
+args = parser.parse_args()
+
+outcomes_20ms = load_outcomes(args.timeline)
+
+fig, ax = plt.subplots(figsize=(5, 4.3))
+fig.tight_layout(pad=4.0)
+plot_speed_graph(outcomes_20ms, ax)
+plt.savefig("ratio.pdf", format="pdf")
